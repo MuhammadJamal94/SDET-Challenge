ab0369e9bd6e4b483e05ed71c1b5256e721d79dd
Mohamad Hasanien <muhammadsolais@gmail.com>
2024-11-04 11:27:58 +0100

    update frontend folder structure

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,30 +1,30 @@
-import { PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-    use: {
-        baseURL: 'http://localhost:3000',
-        headless: true,
-        viewport: { width: 1280, height: 720 },
-        ignoreHTTPSErrors: true,
-        video: 'retain-on-failure',
-        screenshot: 'only-on-failure',
+  use: {
+    baseURL: "http://localhost:3000",
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: "retain-on-failure",
+    screenshot: "only-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
     },
-    projects: [
-        {
-            name: 'chromium',
-            use: { browserName: 'chromium' },
-        },
-        // {
-        //     name: 'firefox',
-        //     use: { browserName: 'firefox' },
-        // },
-        // {
-        //     name: 'webkit',
-        //     use: { browserName: 'webkit' },
-        // },
-    ],
-    reporter: 'html',
-    timeout: 30000,
+    // {
+    //     name: 'firefox',
+    //     use: { browserName: 'firefox' },
+    // },
+    // {
+    //     name: 'webkit',
+    //     use: { browserName: 'webkit' },
+    // },
+  ],
+  reporter: [["html", { outputFolder: "tests/playwright-report" }]],
+  timeout: 30000,
 };
 
 export default config;
